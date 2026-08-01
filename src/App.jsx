dac0f377b8b819ff378ea7d8dcf23a8041ca7e13
ht1pst <funtoobadua@gmail.com>
2026-08-01 11:58:16 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -79,7 +79,18 @@ function App() {
             }
           />
 
-          
+          <Route
+            path="/about"
+            element={
+              <>
+                <AboutSec1 />
+                <AboutSec2 />
+                <AboutSec3 />
+                <AboutSec4 />
+                <Footer />
+              </>
+            }
+          />
         </Routes>
       </main>
     </Router>
