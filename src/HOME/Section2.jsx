635550d55d9acdfe22993a14238a16193916b79d
ht1pst <funtoobadua@gmail.com>
2026-08-17 14:59:57 +0100

    commit

--- src/HOME/Section2.jsx
+++ src/HOME/Section2.jsx
@@ -199,7 +199,7 @@ initial={{ opacity: 0, y: 40 }}       // starts slightly below and invisible
   }}
   style={{ transformOrigin: "center center" }}
 >
-  Aboutgt
+  About Us
 </motion.button>
     </motion.div>
 
